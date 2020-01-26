345fec5efef6e9f4c7f80390c2b92e1a8628cc10
Prab <54489997+NotPrab@users.noreply.github.com>
2020-01-25 20:42:49 -0800

    Add files via upload

---
+++ WINForm .NET Fraemwork/TiTleRandom/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TiTleRandom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RandomStrings_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            StringBuilder builder = new StringBuilder();
+            int num = 1;
+            do
+            {
+                int StartIndex = random.Next(0, 30);
+                builder.Append("abcedfghidjkmlnop1234567890!@#$%^&*þЯäßρ®ØŦ∑¢ŦOя".Substring(StartIndex, 1));
+                num += 1;
+            }
+            while ((num <= 10));
+            this.Text = builder.ToString();
+        }
+        
+    }
+}
