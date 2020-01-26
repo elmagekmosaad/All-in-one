c788fb1a1bd34dd02cc45c278e194a035bc34a27
Prab <54489997+NotPrab@users.noreply.github.com>
2020-01-25 21:12:50 -0800

    Add files via upload

---
+++ WINForm .NET Fraemwork/GenerateSerialKey/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GenerateSerialKey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.txtSerial = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnValid = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Day";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Serial";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Status";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(164, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password";
+            // 
+            // txtDay
+            // 
+            this.txtDay.Location = new System.Drawing.Point(58, 34);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(76, 20);
+            this.txtDay.TabIndex = 4;
+            this.txtDay.Text = "30";
+            // 
+            // txtSerial
+            // 
+            this.txtSerial.Location = new System.Drawing.Point(58, 94);
+            this.txtSerial.Name = "txtSerial";
+            this.txtSerial.Size = new System.Drawing.Size(251, 20);
+            this.txtSerial.TabIndex = 5;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(69, 152);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(338, 89);
+            this.txtStatus.TabIndex = 6;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(223, 34);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(126, 20);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(315, 94);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 22);
+            this.btnGenerate.TabIndex = 8;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnValid
+            // 
+            this.btnValid.Location = new System.Drawing.Point(420, 251);
+            this.btnValid.Name = "btnValid";
+            this.btnValid.Size = new System.Drawing.Size(75, 22);
+            this.btnValid.TabIndex = 9;
+            this.btnValid.Text = "Valid";
+            this.btnValid.UseVisualStyleBackColor = true;
+            this.btnValid.Click += new System.EventHandler(this.btnValid_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 279);
+            this.Controls.Add(this.btnValid);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtSerial);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generate Serial Key";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDay;
+        private System.Windows.Forms.TextBox txtSerial;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnValid;
+    }
+}
+
---
+++ WINForm .NET Fraemwork/GenerateSerialKey/Form1.cs
@@ -0,0 +1,41 @@
+﻿#region Settings
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#endregion
+/// <summary>
+/// Source / Tutorial : https://www.youtube.com/watch?v=ReA_SuPD7cU
+/// </summary>
+namespace GenerateSerialKey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // Generate button
+            SKGL.Generate generate = new SKGL.Generate();
+            generate.secretPhase = txtPassword.Text;
+            txtSerial.Text = generate.doKey(Convert.ToInt32(txtDay.Text));
+        }
+
+        private void btnValid_Click(object sender, EventArgs e)
+        {
+            // Valid Button
+            SKGL.Validate valide = new SKGL.Validate();
+            valide.secretPhase = txtPassword.Text;
+            valide.Key = txtSerial.Text;
+            txtStatus.Text = "Create Date:" + valide.CreationDate + "\r\n" + "ExpireDate:" + valide.ExpireDate + "\r\n" + "DayLeft:" + valide.DaysLeft;
+        }
+    }
+}
